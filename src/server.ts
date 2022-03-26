fa690cf7c2eef66ae8d6fb1fb6109f9d74ee05c6
wslmacieira <wslmacieira@outlook.com>
2022-03-26 12:28:22 -0300

    iniciando a API com OvernightJS e express

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import { Server } from '@overnightjs/core';
+import express, { Application } from 'express';
+import { ForecastController } from './controllers/forecast';
+import './util/module-alias';
+
+export class SetupServer extends Server {
+  constructor(private port = 3000) {
+    super();
+  }
+
+  public init(): void {
+    this.setupExpress();
+    this.setupControllers()
+  }
+
+  public getApp(): Application {
+    return this.app;
+  }
+
+  private setupExpress(): void {
+    this.app.use(express.json());
+  }
+
+  private setupControllers(): void {
+    const forecastController = new ForecastController();
+    this.addControllers([forecastController])
+  }
+}
