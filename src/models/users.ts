99b6f0f8a1777eddffbd58987757ab71838e0e44
wslmacieira <wslmacieira@outlook.com>
2022-03-27 20:44:40 -0300

    Módulo de Users - AuthService, bcrypt e Jest Matchers

--- src/models/users.ts
+++ src/models/users.ts
@@ -1,4 +1,5 @@
 import mongoose, { Document, Model } from 'mongoose';
+import AuthService from '@src/services/auth';
 
 export interface User {
   _id?: string;
@@ -45,5 +46,16 @@ schema.path('email').validate(
   'already exists in the database.',
   CUSTOM_VALIDATION.DUPLICATED
 );
+
+schema.pre<UserModel>('save', async function (): Promise<void> {
+  if (!this.password || !this.isModified()) {
+    return;
+  }
+  try {
+    this.password = await AuthService.hashPassword(this.password);
+  } catch (err) {
+    console.error(`Error hashing the password for the user ${this.name}`);
+  }
+});
 
 export const User: Model<UserModel> = mongoose.model('User', schema);
