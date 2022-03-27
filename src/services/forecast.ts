20b145f026a6b40803af7bc98f43562d2530f4ab
wslmacieira <wslmacieira@outlook.com>
2022-03-27 12:40:39 -0300

    Adicionando o Beach Model com Mongoose e Typescript

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -1,20 +1,6 @@
 import { ForecastPoint, StormGlass } from '../clients/stormGlass';
+import { Beach } from '../models/beach';
 import { InternalError } from '../util/errors/internal-error';
-
-export enum BeachPosition {
-  S = 'S',
-  E = 'E',
-  W = 'W',
-  N = 'N',
-}
-
-export interface Beach {
-  lat: number;
-  lng: number;
-  name: string;
-  position: BeachPosition;
-  user: string;
-}
 
 export interface TimeForecast {
   time: string;
