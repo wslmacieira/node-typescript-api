27fa322bae85d7b8d84a28a79e8c8b19325b8540
wslmacieira <wslmacieira@outlook.com>
2022-03-27 17:36:26 -0300

    Módulo de Users - Criando Controller e Model

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import express, { Application } from 'express';
 import { ForecastController } from './controllers/forecast';
 import { BeachesController } from './controllers/beaches';
 import * as database from '@src/database';
+import { UsersController } from './controllers/users';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
@@ -27,7 +28,12 @@ export class SetupServer extends Server {
   private setupControllers(): void {
     const forecastController = new ForecastController();
     const beachesController = new BeachesController();
-    this.addControllers([forecastController, beachesController]);
+    const usersController = new UsersController();
+    this.addControllers([
+      forecastController,
+      beachesController,
+      usersController,
+    ]);
   }
 
   private async databaseSetup(): Promise<void> {
---
+++ test/functional/users.test.ts
@@ -0,0 +1,17 @@
+import { User } from '@src/models/users';
+
+describe('Users functional tests', () => {
+  beforeAll(async () => await User.deleteMany({}));
+  describe('When creating a new user', () => {
+    it('should successfully create a new user', async () => {
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@email.com',
+        password: '1234',
+      };
+      const response = await global.testRequest.post('/users').send(newUser);
+      expect(response.status).toBe(201);
+      expect(response.body).toEqual(expect.objectContaining(newUser));
+    });
+  });
+});
