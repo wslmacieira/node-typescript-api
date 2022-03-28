82cd483d543db3f68c4f5d1836a746346b687d47
wslmacieira <wslmacieira@outlook.com>
2022-03-27 21:47:18 -0300

    Módulo de Users - Json Web Token JWT com Node.js

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -2,6 +2,7 @@ import { Controller, Post } from '@overnightjs/core';
 import { Request, Response } from 'express';
 import { User } from '@src/models/users';
 import { BaseController } from '.';
+import AuthService from '../services/auth';
 
 @Controller('users')
 export class UsersController extends BaseController {
@@ -14,5 +15,28 @@ export class UsersController extends BaseController {
     } catch (error) {
       this.sendCreatedUpdatedErrorResponse(res, error);
     }
+  }
+
+  @Post('authenticate')
+  public async authenticate(
+    req: Request,
+    res: Response
+  ): Promise<Response | void> {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).send({
+        code: 401,
+        error: 'User not found!',
+      });
+    }
+    if (!(await AuthService.comparePassword(password, user.password))) {
+      return res.status(401).send({
+        code: 401,
+        error: 'Password or email does not match!',
+      });
+    }
+    const token = AuthService.generateToken(user.toJSON());
+    return res.status(200).send({ token });
   }
 }
